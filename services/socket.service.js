a168f9d7875f769d033fae9c37551a01a8940e6a
Mansei <yarden835@gmail.com>
2022-01-27 14:36:00 +0200

    sockets

--- services/socket.service.js
+++ services/socket.service.js
@@ -14,31 +14,31 @@ function connectSockets(http, session) {
         socket.on('disconnect', socket => {
             console.log('Someone disconnected')
         })
-        socket.on('chat topic', topic => {
-            if (socket.myTopic === topic) return;
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
+        socket.on('enter board', boardId => {
+            if (socket.board === boardId) return;
+            if (socket.board) {
+                socket.leave(socket.boardId)
             }
-            socket.join(topic)
-            socket.myTopic = topic
+            socket.join(boardId)
+            socket.board = boardId
         })
-        socket.on('chat newMsg', msg => {
-            console.log('Emitting Chat msg', msg);
+        socket.on('update board', boardId => {
+            console.log('Emitting new board', boardId);
             // emits to all sockets:
             // gIo.emit('chat addMsg', msg)
             // emits only to sockets in the same room
-            gIo.to(socket.myTopic).emit('chat addMsg', msg)
+            gIo.to(socket.board).emit('updated board', boardId)
         })
-        socket.on('user-watch', userId => {
-            socket.join('watching:' + userId)
-        })
-        socket.on('set-user-socket', userId => {
-            logger.debug(`Setting (${socket.id}) socket.userId = ${userId}`)
-            socket.userId = userId
-        })
-        socket.on('unset-user-socket', () => {
-            delete socket.userId
-        })
+        // socket.on('user-watch', userId => {
+        //     socket.join('watching:' + userId)
+        // })
+        // socket.on('set-user-socket', userId => {
+        //     logger.debug(`Setting (${socket.id}) socket.userId = ${userId}`)
+        //     socket.userId = userId
+        // })
+        // socket.on('unset-user-socket', () => {
+        //     delete socket.userId
+        // })
 
     })
 }
